50dcc7674266d4823d835807d55ff1cc33feeeb8
Ryan <rocifier@gmail.com>
2012-09-24 16:44:30 +1200

    1
    1

--- js/entities.js
+++ js/entities.js
@@ -11,17 +11,26 @@ var PlayerEntity = me.ObjectEntity.extend({
     ------ */
  
     init: function(x, y, settings) {
-        // call the constructor
         this.parent(x, y, settings);
  
- 		// set default acceleration
+ 		// set default acceleration and max velocity
  		this.setVelocity(5, 5);
- 
+ 		
+ 		// make it collidable
+        this.collidable = true;
+        
         // set the display to follow our position on both axis
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
- 
+        
+        // set pathing variables
+        this.isPathing = false;
+        this.pathingIndex = 0;
+        this.path = [];
+        this.busyWalking = false;
+        
+ 		console.log('player created.');
     },
- 
+    
     /* -----
  
     update the player pos
@@ -49,6 +58,15 @@ var PlayerEntity = me.ObjectEntity.extend({
             this.vel.y = 0;
         }
  
+		// check for collision
+		var res = me.game.collide(this);
+		if (res && res.obj.type == me.game.ENEMY_OBJECT) {
+			console.log('hit');
+		}
+		
+		// pathfinding movement
+		this.doPathing();
+
         // check & update player movement
         this.updateMovement();
  
@@ -62,7 +80,51 @@ var PlayerEntity = me.ObjectEntity.extend({
         // else inform the engine we did not perform
         // any update (e.g. position, animation)
         return false;
-    }
+    },
+    
+    pathTo: function(dest) {
+    	var runnerPos = jsApp.runner.pos;
+    	var src = jsApp.getTilePosition(runnerPos.x, runnerPos.y);
+    	
+        var a = jsApp.graph.nodes[src.x][src.y];
+        var b = jsApp.graph.nodes[dest.x][dest.y];
+        this.path = astar.search(jsApp.graph.nodes, a, b, true);
+        
+        if (this.path.length == 0) {
+        	return false;  // no modification to path because the new one is invalid
+        } else {
+        	this.isPathing = true;  // Begin new path..
+        	this.pathingIndex = 0;  // Starting at node 0
+        }
+
+    	return true;
+    },
+    
+    doPathing: function() {
+    	if (this.isPathing) {
+    		if (!this.busyWalking) {
+    			jsApp.runner.busyWalking = true;
+				var dest = this.path[this.pathingIndex];
+				this.destPixels = jsApp.getTileCoord(dest.x, dest.y);
+				
+				tween = new me.Tween(this.pos)
+					.to({x: this.destPixels.x, y: this.destPixels.y}, 100)
+					.onComplete( function() {
+						if (jsApp.runner.isPathing) {
+							jsApp.runner.busyWalking = false;
+							jsApp.runner.pathingIndex++;
+							if (jsApp.runner.pathingIndex >= jsApp.runner.path.length) {
+								// reached final destination
+								jsApp.runner.pathingIndex = 0;
+								jsApp.runner.isPathing = false;
+							}
+						}
+					});
+				tween.start();
+			}
+		}
+    },
+    
  
 });
 
@@ -73,31 +135,44 @@ an enemy turret
 ------------------------ */
 var TurretEntity = me.ObjectEntity.extend({
 
-	MissileEntity missile;
-	
     init: function(x, y, settings) {
 
-        // call the parent constructor
+		settings.image = "turret1";
+		settings.spritewidth = 21;
+		settings.spriteheight = 21;
+		
         this.parent(x, y, settings);
 
         // make it collidable
         this.collidable = true;
-        // make it a enemy object
-        this.type = me.game.ENEMY_OBJECT;
- 
- 		missile = new MissileEntity();
- 		
+
+ 		this.spawnedBullet = false;
+
     },
- 
+    
     // manage the enemy movement
     update: function() {
-    
-    	// handle projectiles
+    	this.parent();
     	
+    	if (!this.spawnedBullet) {
+    		this.spawnedBullet = true;
+    		var missile = new MissileEntity(this.pos.x+8, this.pos.y, { image: 'missile2', spritewidth: 7, spriteheight: 7 });
+ 			me.game.add(missile, this.z);
+ 			me.game.sort();
+ 		}
+ 
+		// check for collision
+		var res = me.game.collide(this);
+		if (res && res.obj.type == me.game.ENEMY_OBJECT) {
+			
+		}
+
         // check and update movement
         this.updateMovement();
 
-        return false;
+		this.parent(this);
+		
+        return true;
     }
 });
 
@@ -107,36 +182,55 @@ an enemy missile
 ------------------------ */
 var MissileEntity = me.ObjectEntity.extend({
 
-	var missile;
-	
     init: function(x, y, settings) {
-
+		this.startx = x;
+		this.starty = y;
+		
 		// todo: image angle
-		settings.image = "missile1";
 
-        // call the parent constructor
         this.parent(x, y, settings);
 
+		// set default acceleration
+ 		this.setVelocity(0, 2);
+ 		
         // make it collidable
         this.collidable = true;
+        this.collidedWithSolids = 0;
         
         // make it a enemy object
         this.type = me.game.ENEMY_OBJECT;
  
     },
     
-    // call by the engine when colliding with another object
-    // obj parameter corresponds to the other object (typically the player) touching this one
-    onCollision: function(res, obj) {
- 
-    },
-    
     // manage the enemy movement
     update: function() {
 
-        // check and update movement
-        this.updateMovement();
+		if (!this.visible){
+			// remove myself if not on the screen anymore
+            //me.game.remove(this);
+		}
+		
+		// update the entity velocity
+        this.vel.y -= this.accel.y * me.timer.tick;
+        
+		// check for collision with player
+		var res = me.game.collide(this);
+		if (res) {
+			this.collidedWithSolids++;
+			
+			//console.log('bullet hit');
+		}
 
+        // check and update movement
+        var res = this.updateMovement();
+        if (res.x != 0 || res.y !=0 ) {
+			this.pos.x = this.startx;
+ 			this.pos.y = this.starty;
+ 			this.vel.y = 0;
+		}
+		
+		this.parent(this);
+		
         return true;
     }
 });
--- js/main.js
+++ js/main.js
@@ -29,6 +29,14 @@ var g_resources = [{
     type: "image",
     src: "data/img/missile1.png"
 }, {
+    name: "missile2",
+    type: "image",
+    src: "data/img/missile2.png"
+}, {
+    name: "turret1",
+    type: "image",
+    src: "data/img/turret1.png"
+}, {
     name: "walk",
     type: "image",
     src: "data/img/walk.gif"
@@ -36,6 +44,9 @@ var g_resources = [{
 
 var jsApp	= 
 {	
+	grid: [],
+	graph: [],
+	
 	/* ---
 	
 		Initialize the jsApp
@@ -77,6 +88,10 @@ var jsApp	=
 
 		// add our player entity in the entity pool
 	   me.entityPool.add("runner", PlayerEntity);
+	   me.entityPool.add("turret1", TurretEntity);
+	   me.entityPool.add("turret2", TurretEntity);
+	   me.entityPool.add("turret3", TurretEntity);
+	   me.entityPool.add("turret4", TurretEntity);
 	   
 	   // enable the keyboard
 	   me.input.bindKey(me.input.KEY.LEFT,  "left");
@@ -86,13 +101,124 @@ var jsApp	=
       
         // disable gravity
         me.sys.gravity = 0;
+        //me.debug.renderHitBox = true;
         
       // start the game 
 		me.state.change(me.state.PLAY);
 		
+	},
+	
+  // calculate angle between 2 points  
+  calcAngle: function(x1, x2, y1, y2) { 
+    return -Math.atan2(y2 - y1, x2 - x1);
+  },
+  
+  // calculate x velocity on angle
+  calcVelX: function(vel, angle){
+    return -vel * Math.sin(angle);
+  },
+  
+  // calculate y velocity on angle
+  calcVelY: function(vel, angle){
+    return vel * Math.cos(angle);
+  },
+  
+  onMouseClick: function(x, y) {
+  	
+  	if (me.state.isCurrent(me.state.PLAY)) {
+  	
+		// compensate for camera position
+		x += me.game.viewport.pos.x;
+		y += me.game.viewport.pos.y;
+		
+		// pick tiles
+		var pos = jsApp.getTilePosition(x, y);
+		var tilesolid = jsApp.isTileSolid(x, y);
+		
+		if (!tilesolid) {
+			// try to walk there
+			if (!this.runner.pathTo(pos)) {
+				// make bleep error sound and show "cannot walk here. path blocked."
+			}
+		} else {
+			// make bleep error sound and show "cannot walk here. select a floor location."
+		}
+    }
+    
+  },
+  
+  // get tile row and col from pixels
+  getTilePosition: function(x, y) {
+    var pos = [];
+    pos.x = Math.floor(x / me.game.currentLevel.collisionLayer.tilewidth);
+    pos.y = Math.floor(y / me.game.currentLevel.collisionLayer.tileheight);  
+    return pos;
+  },
+  
+  // get tile position in pixels from pixels
+  getTilePosPixels: function(x, y) {
+    var tilePos = jsApp.getTilePosition(x, y);
+    var pos = [];
+    pos.x = tilePos.x * me.game.currentLevel.collisionLayer.tilewidth;
+    pos.y = tilePos.y * me.game.currentLevel.collisionLayer.tileheight;
+    return pos;
+  },
+  
+  // get tile position in pixels from row and col
+  getTileCoord: function(x, y) {
+    var pos = [];
+    pos.x = x * me.game.currentLevel.collisionLayer.tilewidth;
+    pos.y = y * me.game.currentLevel.collisionLayer.tileheight;
+    return pos;
+  },
+  
+  isTileSolid: function(x, y) {
+    var tilePos = jsApp.getTilePosition(x, y);
+    if (me.game.currentLevel.collisionLayer.layerData[~~(tilePos.x)]) {
+      var collisionTile = me.game.currentLevel.collisionLayer.layerData[~~(tilePos.x)][~~(tilePos.y)];            
+      if (collisionTile != null && collisionTile != undefined) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }    
+  },
+
+	loadMaze: function(mazename)
+	{
+		me.levelDirector.loadLevel(mazename);
+		me.game.sort();
+		
+		jsApp.runner = me.game.getEntityByName('runner')[0];
+		me.game.currentLevel.collisionLayer = me.game.currentLevel.getLayerByName("collision");
+		
+		// Setup A* graph
+        var e = me.game.currentLevel.collisionLayer;
+        if (!e.layerData) {
+            alert("Error: Collision layer missing!");
+            return;
+        }
+        var g = e.height;
+        var f = e.width;
+        for (var d = 0; d < f; d += 1) {
+            jsApp.grid[d] = [];
+            for (var c = 0; c < g; c += 1) {
+                var b = jsApp.getTileCoord(d, c);
+                if (e.getTile(b.x, b.y) == null) {
+                    jsApp.grid[d][c] = 0;
+                } else {
+                    jsApp.grid[d][c] = 1;
+                }
+            }
+        }
+        jsApp.graph = new Graph(jsApp.grid);
+        
 	}
 
 }; // jsApp
+
+
 
 /* the in game stuff*/
 var PlayScreen = me.ScreenObject.extend(
@@ -102,16 +228,15 @@ var PlayScreen = me.ScreenObject.extend(
 	{	
       // stuff to reset on state change
       // load a level
-        me.levelDirector.loadLevel("Test");
-        
+      jsApp.loadMaze("Test");
+
+    	// result is an array containing the shortest path
 	},
 	
 	
 	/* ---
-	
-		 action to perform when game is finished (state change)
-		
-		---	*/
+	action to perform when game is finished (state change)
+	---	*/
 	onDestroyEvent: function()
 	{
 	
@@ -124,4 +249,13 @@ var PlayScreen = me.ScreenObject.extend(
 window.onReady(function() 
 {
 	jsApp.onload();
+	
+	
+	$('canvas').click( function(e) {
+	  var jsappOffset = $('#jsapp').offset();  
+	  var mouseX = e.pageX - jsappOffset.left;
+	  var mouseY = e.pageY - jsappOffset.top;
+	  jsApp.onMouseClick(mouseX, mouseY);
+	});
+	
 });
